2d42ec3bc6f14bc333aec32b0f06f0f60c19d79c
Chenxiih <127761039+Chenxiih@users.noreply.github.com>
2023-06-09 18:47:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,23 +43,19 @@ class Trie:
         return t[0:5]
     
 @st.experimental_singleton
-    def decompress_pickle(file):
+def decompress_pickle(file):
         data = bz2.BZ2File(file, 'rb')
         data = pickle.load(data)
 
         return data
 
-    model_path = 'trie_pkl.pbz2'
-    trie = decompress_pickle(model_path)
+model_path = 'trie_pkl.pbz2'
+trie = decompress_pickle(model_path)
 
-    def main():
-        st.title("Welcome to VeePee")
-        user_input = st.text_input("Start typing here...")
+st.title("Welcome to VeePee")
+user_input = st.text_input("Start typing here...")
 
-        if user_input:
+if user_input:
             results = trie.autocomple(user_input.lower())
             for result in results:
                 st.write(result)
-
-    if __name__ == "__main__":
-        main()
